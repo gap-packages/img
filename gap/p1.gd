925d0a4a1cbb193caf71198ab156f502fd44c83e
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2013-05-07 00:13:12 +0200

    Documented sphere and p1

--- gap/p1.gd
+++ gap/p1.gd
@@ -2,9 +2,7 @@
 ##
 #W p1.gd                                                    Laurent Bartholdi
 ##
-#H   @(#)$Id$
-##
-#Y Copyright (C) 2012, Laurent Bartholdi
+#Y Copyright (C) 2012-2013, Laurent Bartholdi
 ##
 #############################################################################
 ##
@@ -15,10 +13,22 @@
 ## <#GAPDoc Label="P1Points">
 ## <ManSection>
 ##   <Filt Name="IsP1Point"/>
+DeclareCategory("IsP1Point",IsObject);
+DeclareCategoryCollections("IsP1Point");
+DeclareCategoryCollections("IsP1PointCollection");
+DeclareSynonym("IsP1PointList",IsP1PointCollection and IsList);
+DeclareCategory("IsIEEE754P1Point",IsP1Point);
 ##   <Fam Name="P1PointsFamily"/>
+BindGlobal("P1PointsFamily",NewFamily("P1PointsFamily",IsP1Point));
+BindGlobal("TYPE_P1POINT",NewType(P1PointsFamily,IsP1Point and IsPositionalObjectRep));
+BindGlobal("TYPE_IEEE754P1POINT",NewType(P1PointsFamily,IsIEEE754P1Point and IsDataObjectRep));
 ##   <Func Name="P1Point" Arg="complex"/>
 ##   <Func Name="P1Point" Arg="real, imag" Label="ri"/>
 ##   <Func Name="P1Point" Arg="string" Label="s"/>
+DeclareOperation("P1Point",[IsFloat]);
+DeclareOperation("P1Point",[IsRat]);
+DeclareOperation("P1Point",[IsInfinity]);
+DeclareOperation("P1Point",[IsFloat,IsFloat]);
 ##   <Description>
 ##     P1 points are complex numbers or infinity;
 ##     fast methods are implemented to compute with them, and to apply
@@ -30,69 +40,97 @@
 ## </ManSection>
 ##
 ## <ManSection>
+##   <Func Name="P1Coordinate" Arg="p"/>
+DeclareAttribute("P1Coordinate",IsP1Point);
+##   <Returns>The complex number represented by <A>p</A>.</Returns>
+## </ManSection>
+##
+
+## <ManSection>
 ##   <Func Name="CleanedP1Point" Arg="p, [prec]"/>
+DeclareOperation("CleanedP1Point",[IsP1Point,IsFloat]);
+DeclareOperation("CleanedP1Point",[IsP1Point]);
 ##   <Returns><A>p</A>, rounded towards 0/1/infinity/reals at precision <A>prec</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Var Name="P1infinity"/>
+DeclareGlobalVariable("P1infinity");
+DeclareOperation("P1INFINITY@",[IsP1Point]);
 ##   <Var Name="P1one"/>
+DeclareGlobalVariable("P1one");
 ##   <Var Name="P1zero"/>
+DeclareGlobalVariable("P1zero");
 ##   <Description>The south, north and 'east' poles of the Riemann sphere.</Description>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="P1Antipode" Arg="p"/>
+DeclareAttribute("P1Antipode",IsP1Point);
 ##   <Returns>The antipode of <A>p</A> on the Riemann sphere.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="P1Barycentre" Arg="points ..."/>
+DeclareOperation("P1Barycentre",[IsP1PointList]);
+DeclareOperation("P1Barycentre",[IsP1Point]);
+DeclareOperation("P1Barycentre",[IsP1Point,IsP1Point]);
+DeclareOperation("P1Barycentre",[IsP1Point,IsP1Point,IsP1Point]);
 ##   <Returns>The barycentre of its arguments (which can also be a list of P1 points).</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="P1Circumcentre" Arg="p, q, r"/>
+DeclareOperation("P1Circumcentre",[IsP1Point,IsP1Point,IsP1Point]);
 ##   <Returns>The centre of the smallest disk containing <A>p,q,r</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="P1Distance" Arg="p, q"/>
+DeclareOperation("P1Distance",[IsP1Point,IsP1Point]);
 ##   <Returns>The spherical distance from <A>p</A> to <A>q</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="P1Midpoint" Arg="p, q"/>
+DeclareOperation("P1Midpoint",[IsP1Point,IsP1Point]);
 ##   <Returns>The point between <A>p</A> to <A>q</A> (undefined if they are antipodes of each other).</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="P1Sphere" Arg="v"/>
+DeclareAttribute("P1Sphere",IsList);
 ##   <Returns>The P1 point corresponding to <A>v</A> in <M>\mathbb R^3</M>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="SphereP1" Arg="p"/>
+DeclareAttribute("SphereP1",IsP1Point);
 ##   <Returns>The coordinates in <M>\mathbb R^3</M> of <A>p</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="SphereP1Y" Arg="p"/>
+DeclareAttribute("SphereP1Y",IsP1Point);
 ##   <Returns>The Y coordinate in <M>\mathbb R^3</M> of <A>p</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="P1XRatio" Arg="p q r s"/>
-##   <Returns>The cross ratio of <A>p, q, r, s</A>, as a P1 point.</Returns>
+##   <Func Name="XRatio" Arg="p q r s"/>
+DeclareOperation("P1XRatio",[IsP1Point,IsP1Point,IsP1Point,IsP1Point]);
+##   <Returns>The cross ratio of <A>p, q, r, s</A>.</Returns>
 ##   <Description>
-##     The cross ratio of four points <A>p,q,r,s</A> is defined as <C>(p-r)(q-s)/(p-s)(q-r)</C>.
-##     The values <C>P1zero,P1one,P1infinity</C> correspond respectively to the special cases
+##     The cross ratio of four points <A>p,q,r,s</A> is defined as
+##     <C>(p-r)(q-s)/(p-s)(q-r)</C>. The values <C>P1zero,P1one,P1infinity</C>
+##     correspond respectively to the special cases
 ##     <C>(p=r or q=s)</C>, <C>(p=q or r=s)</C>, <C>(p=s or q=r)</C>.
 ##   </Description>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="XRatio" Arg="p q r s"/>
+DeclareOperation("XRatio",[IsP1Point,IsP1Point,IsP1Point,IsP1Point]);
 ##   <Returns>The cross ratio of <A>p, q, r, s</A>, as a complex number.</Returns>
 ##   <Description>
 ##     The cross ratio of four points <A>p,q,r,s</A> is defined as <C>(p-r)(q-s)/(p-s)(q-r)</C>.
@@ -103,6 +141,8 @@
 ##
 ## <ManSection>
 ##   <Oper Name="CollectedP1Points" Arg="p1points [,precision]"/>
+DeclareOperation("CollectedP1Points",[IsP1PointList]);
+DeclareOperation("CollectedP1Points",[IsP1PointList,IsFloat]);
 ##   <Returns>A list of pairs <C>[point,multiplicity]</C>.</Returns>
 ##   <Description>
 ##     Collects the points in <A>p1points</A>; points at distance at most <A>precision</A>
@@ -113,6 +153,8 @@
 ##
 ## <ManSection>
 ##   <Oper Name="MatchP1Points" Arg="p1pointsA, p1pointsB [,separation]"/>
+DeclareOperation("MatchP1Points",[IsP1PointList,IsP1PointCollColl,IsFloat]);
+DeclareOperation("MatchP1Points",[IsP1PointList,IsP1PointCollColl]);
 ##   <Returns>A list of giving closest point in <A>p1pointsB</A> to points in <A>p1pointsA</A>, or <K>fail</K>.</Returns>
 ##   <Description>
 ##     Finds for each point <A>p1pointsA[i]</A> the closest point <A>p1pointB[p[i]]</A>.
@@ -124,13 +166,12 @@
 ##
 ## <ManSection>
 ##   <Filt Name="IsP1Map"/>
-##   <Fam Name="P1MapsFamily"/>
+DeclareSynonym("IsP1Map",IsUnivariateRationalFunction and IsFloatRationalFunction);
+DeclareCategory("IsIEEE754P1Map",IsP1Map);
+BindGlobal("TYPE_IEEE754P1MAP", NewType(RationalFunctionsFamily(PMCOMPLEX_PSEUDOFIELD), IsIEEE754P1Map and IsDataObjectRep));
 ##   <Description>
 ##     P1 maps are stored more efficiently than rational functions, but are
 ##     otherwise equivalent.
-##     <P/>
-##     The first filter recognizes these objects. Next, the family they
-##     belong to. 
 ##   </Description>
 ## </ManSection>
 ##
@@ -140,6 +181,12 @@
 ##   <Func Name="MoebiusMap" Arg="p, q, r" Label="3"/>
 ##   <Func Name="MoebiusMap" Arg="p, q" Label="2"/>
 ##   <Func Name="MoebiusMap" Arg="p" Label="1"/>
+DeclareOperation("MoebiusMap",[IsP1Point]);
+DeclareOperation("MoebiusMap",[IsP1Point,IsP1Point]);
+DeclareOperation("MoebiusMap",[IsP1Point,IsP1Point,IsP1Point]);
+DeclareOperation("MoebiusMap",[IsP1Point,IsP1Point,IsP1Point,IsP1Point,IsP1Point,IsP1Point]);
+DeclareOperation("MoebiusMap",[IsP1PointList]);
+DeclareOperation("MoebiusMap",[IsP1PointList,IsP1PointList]);
 ##   <Returns>A new Möbius transformation.</Returns>
 ##   <Description>
 ##     In the first case, this is the Möbius transformation sending <A>p,q,r</A> to <A>P,Q,R</A>
@@ -153,6 +200,8 @@
 ##
 ## <ManSection>
 ##   <Oper Name="P1MapRotatingP1Points" Arg="points [oldpoints]"/>
+DeclareOperation("P1ROTATION@",[IsP1Point,IsP1PointList,IsP1PointList]);
+DeclareGlobalFunction("P1MapRotatingP1Points");
 ##   <Returns>A Möbius rotation sending the last of <A>points</A> to P1infinity.</Returns>
 ##   <Description>
 ##     A Möbius rotation is computed that sends the last of <A>points</A> to <K>P1infinity</K> and,
@@ -163,6 +212,8 @@
 ##
 ## <ManSection>
 ##   <Oper Name="P1MapNormalizingP1Points" Arg="points [oldpoints]"/>
+DeclareOperation("P1MapNormalizingP1Points",[IsP1PointList]);
+DeclareOperation("P1MapNormalizingP1Points",[IsP1PointList,IsP1PointList]);
 ##   <Returns>A Möbius transformation sending the last of <A>points</A> to P1infinity.</Returns>
 ##   <Description>
 ##     A Möbius transformation is computed that sends the last of <A>points</A> to
@@ -176,92 +227,142 @@
 ##
 ## <ManSection>
 ##   <Var Name="P1z"/>
+DeclareGlobalVariable("P1z");
 ##   <Description>The identity Möbius transformation.</Description>
 ## </ManSection>
 ##
 ## <ManSection>
+##   <Func Name="P1Monomial" Arg="n"/>
+DeclareGlobalFunction("P1Monomial");
+##   <Returns>The rational function <M>z^n</M>.</Returns>
+## </ManSection>
+##
+## <ManSection>
 ##   <Oper Name="CleanedP1Map" Arg="map, [prec]"/>
+DeclareOperation("CleanedP1Map",[IsP1Map,IsFloat]);
+DeclareOperation("CleanedP1Map",[IsP1Map]);
 ##   <Returns><A>map</A>, with coefficients rounded using <A>prec</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Oper Name="CompositionP1Map" Arg="map1, ..."/>
+DeclareSynonym("CompositionP1Map",CompositionMapping2);
 ##   <Returns>The composition of the maps passed as arguments, in the functional (<A>map1</A> last) order.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Oper Name="InverseP1Map" Arg="map"/>
+DeclareSynonym("InverseP1Map",InverseGeneralMapping);
 ##   <Returns>The functional inverse of the Möbius transformation <A>map</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
+##   <Oper Name="ComplexConjugate" Arg="map"/>
+#DeclareAttribute("ComplexConjugate",IsP1Map);
+##   <Returns>The complex conjugated map.</Returns>
+## </ManSection>
+##
+## <ManSection>
 ##   <Oper Name="ConjugatedP1Map" Arg="map, mobius"/>
+DeclareOperation("ConjugatedP1Map",[IsP1Map,IsP1Map]);
 ##   <Returns>The map <C>CompositionP1Map(InverseP1Map(mobius),map,mobius)</C>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Oper Name="CoefficientsOfP1Map" Arg="map"/>
+DeclareAttribute("CoefficientsOfP1Map",IsP1Map);
 ##   <Returns>Coefficients of numerator and denominator of <A>map</A>, lowest degree first.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Oper Name="P1MapByCoefficients" Arg="numer, denom"/>
+DeclareGlobalFunction("P1MapByCoefficients");
+DeclareOperation("P1MAPBYCOEFFICIENTS2@",[IsObject,IsList,IsList]);
 ##   <Returns>The P1 map with numerator coefficients <A>numer</A> and denominator <A>denom</A>, lowest degree first.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
+##   <Attr Name="NumeratorP1Map" Arg="map"/>
+##   <Attr Name="DenominatorP1Map" Arg="map"/>
+DeclareAttribute("NumeratorP1Map",IsP1Map);
+DeclareAttribute("DenominatorP1Map",IsP1Map);
+##   <Returns>The numerator/denominator of <A>map</A>.</Returns>
+## </ManSection>
+##
+## <ManSection>
+##   <Oper Name="P1MapByZerosPoles" Arg="zeros, poles, src, dest"/>
+DeclareOperation("P1MapByZerosPoles",[IsP1PointList,IsP1PointList,IsP1Point,IsP1Point]);
+##   <Returns>The P1 map with specified zeros and poles, and sending <A>src</A> to <A>dest</A>.</Returns>
+## </ManSection>
+##
+## <ManSection>
 ##   <Oper Name="P1Path" Arg="p q"/>
+DeclareOperation("P1Path",[IsP1Point,IsP1Point]);
 ##   <Returns>The P1 map sending <C>0</C> to <A>p</A> and <C>1</C> to <A>q</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Oper Name="DegreeOfP1Map" Arg="map"/>
+DeclareAttribute("DegreeOfP1Map",IsP1Map);
 ##   <Returns>The degree of <A>map</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Oper Name="P1Image" Arg="map, p1point"/>
+DeclareSynonym("P1Image",ImageElm);
+DeclareOperation("ImageElm",[IsP1Map,IsP1Point]);
 ##   <Returns>The image of <A>p1point</A> under <A>map</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Oper Name="P1PreImage" Arg="map, p1point"/>
+DeclareSynonym("P1PreImage",PreImageElm);
+DeclareOperation("PreImageElm",[IsP1Map,IsP1Point]);
 ##   <Returns>The preimage of <A>p1point</A> under <A>map</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Oper Name="P1PreImages" Arg="map, p1point"/>
+DeclareSynonym("P1PreImages",PreImagesElm);
+DeclareOperation("PreImagesElm",[IsP1Map,IsP1Point]);
 ##   <Returns>The preimages of <A>p1point</A> under <A>map</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
-##   <Oper Name="P1MapCriticalPoints" Arg="map"/>
+##   <Oper Name="Primitive" Arg="map"/>
+##   <Oper Name="Derivative" Arg="map"/>
+DeclareAttribute("Primitive",IsP1Map);
+#DeclareAttribute("Derivative",IsP1Map);
+##   <Returns>The [anti]derivative of the rational map <A>map</A>.</Returns>
+## </ManSection>
+##
+## <ManSection>
+##   <Attr Name="CriticalPointsOfP1Map" Arg="map"/>
+DeclareAttribute("CriticalPointsOfP1Map",IsP1Map);
 ##   <Returns>The critical points of <A>map</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
-##   <Oper Name="P1MapRational" Arg="rat"/>
+##   <Oper Name="AsP1Map" Arg="rat"/>
+DeclareAttribute("AsP1Map",IsScalar);
 ##   <Returns>The P1 map given by the rational function <A>rat</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
-##   <Oper Name="RationalP1Map" Arg="map"/>
-##   <Oper Name="RationalP1Map" Arg="indeterminate, map" Label="im"/>
-##   <Returns>The rational function given by P1 map <A>map</A>.</Returns>
-## </ManSection>
-##
-## <ManSection>
 ##   <Oper Name="P1MapSL2" Arg="mat"/>
+DeclareOperation("P1MapSL2",[IsMatrix]);
 ##   <Returns>The Möbius P1 map given by the 2x2 matrix <A>mat</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Oper Name="SL2P1Map" Arg="map"/>
+DeclareAttribute("SL2P1Map",IsP1Map);
 ##   <Returns>The matrix of the Möbius P1 map <A>map</A>.</Returns>
 ## </ManSection>
 ##
 ## <ManSection>
 ##   <Func Name="SetP1Points" Arg="record [prec]"/>
+DeclareGlobalFunction("SetP1Points");
 ##   <Description>
 ##     Installs a default implementation for P1 points. Fundamentally,
 ##     a P1 point is a complex number or infinity, with a few extra
@@ -281,96 +382,10 @@
 ##
 ## <#/GAPDoc>
 
-DeclareGlobalFunction("SetP1Points");
-
-DeclareCategory("IsP1Point",IsObject);
-DeclareCategoryCollections("IsP1Point");
-DeclareCategoryCollections("IsP1PointCollection");
-DeclareSynonym("IsP1PointList",IsP1PointCollection and IsList);
-DeclareCategory("IsIEEE754P1Point",IsP1Point);
-BindGlobal("P1PointsFamily",NewFamily("P1PointsFamily",IsP1Point));
-BindGlobal("TYPE_P1POINT",NewType(P1PointsFamily,IsP1Point and IsPositionalObjectRep));
-BindGlobal("TYPE_IEEE754P1POINT",NewType(P1PointsFamily,IsIEEE754P1Point and IsDataObjectRep));
-
-DeclareOperation("P1Point",[IsFloat]);
-DeclareOperation("P1Point",[IsRat]);
-DeclareOperation("P1Point",[IsInfinity]);
-DeclareOperation("P1Point",[IsFloat,IsFloat]);
-DeclareGlobalVariable("P1infinity");
-DeclareOperation("P1INFINITY@",[IsP1Point]);
-DeclareGlobalVariable("P1one");
-DeclareGlobalVariable("P1zero");
-DeclareOperation("P1Barycentre",[IsP1PointList]);
-DeclareOperation("P1Barycentre",[IsP1Point]);
-DeclareOperation("P1Barycentre",[IsP1Point,IsP1Point]);
-DeclareOperation("P1Barycentre",[IsP1Point,IsP1Point,IsP1Point]);
-DeclareAttribute("SphereP1",IsP1Point);
-DeclareAttribute("SphereP1Y",IsP1Point);
-DeclareAttribute("P1Sphere",IsList);
-DeclareOperation("P1Distance",[IsP1Point,IsP1Point]);
-DeclareOperation("P1Circumcentre",[IsP1Point,IsP1Point,IsP1Point]);
-DeclareOperation("P1XRatio",[IsP1Point,IsP1Point,IsP1Point,IsP1Point]);
-DeclareOperation("XRatio",[IsP1Point,IsP1Point,IsP1Point,IsP1Point]);
-DeclareOperation("CleanedP1Point",[IsP1Point,IsFloat]);
-DeclareOperation("CleanedP1Point",[IsP1Point]);
-DeclareOperation("P1Midpoint",[IsP1Point,IsP1Point]);
-DeclareAttribute("P1Antipode",IsP1Point);
-DeclareAttribute("P1Coordinate",IsP1Point);
-DeclareOperation("CollectedP1Points",[IsP1PointList]);
-DeclareOperation("CollectedP1Points",[IsP1PointList,IsFloat]);
-DeclareOperation("MatchP1Points",[IsP1PointList,IsP1PointCollColl,IsFloat]);
-DeclareOperation("MatchP1Points",[IsP1PointList,IsP1PointCollColl]);
-
-################################################################
-# p1 maps
-################################################################
-DeclareSynonym("IsP1Map",IsUnivariateRationalFunction and IsFloatRationalFunction);
-DeclareCategory("IsIEEE754P1Map",IsP1Map);
-BindGlobal("TYPE_IEEE754P1MAP", NewType(RationalFunctionsFamily(PMCOMPLEX_PSEUDOFIELD), IsIEEE754P1Map and IsDataObjectRep));
-
-DeclareGlobalFunction("P1MapByCoefficients");
-DeclareOperation("P1MAPBYCOEFFICIENTS2@",[IsObject,IsList,IsList]);
-DeclareAttribute("CoefficientsOfP1Map",IsP1Map);
-DeclareAttribute("AsP1Map",IsScalar);
-DeclareOperation("P1MapSL2",[IsMatrix]);
-DeclareAttribute("SL2P1Map",IsP1Map);
-DeclareOperation("P1MapByZerosPoles",[IsP1PointList,IsP1PointList,IsP1Point,IsP1Point]);
-
-DeclareOperation("MoebiusMap",[IsP1Point]);
-DeclareOperation("MoebiusMap",[IsP1Point,IsP1Point]);
-DeclareOperation("MoebiusMap",[IsP1Point,IsP1Point,IsP1Point]);
-DeclareOperation("MoebiusMap",[IsP1Point,IsP1Point,IsP1Point,IsP1Point,IsP1Point,IsP1Point]);
-DeclareOperation("MoebiusMap",[IsP1PointList]);
-DeclareOperation("MoebiusMap",[IsP1PointList,IsP1PointList]);
-DeclareOperation("P1Path",[IsP1Point,IsP1Point]);
-DeclareGlobalVariable("P1z");
-DeclareGlobalFunction("P1Monomial");
-DeclareOperation("CleanedP1Map",[IsP1Map,IsFloat]);
-DeclareOperation("CleanedP1Map",[IsP1Map]);
-
-DeclareAttribute("Primitive",IsP1Map);
-#DeclareAttribute("Derivative",IsP1Map);
-#DeclareAttribute("ComplexConjugate",IsP1Map);
-DeclareAttribute("NumeratorP1Map",IsP1Map);
-DeclareAttribute("DenominatorP1Map",IsP1Map);
-DeclareSynonym("P1Image",ImageElm);
-DeclareSynonym("P1PreImages",PreImagesElm);
-DeclareSynonym("P1PreImage",PreImageElm);
-DeclareAttribute("DegreeOfP1Map",IsP1Map);
-DeclareSynonym("InverseP1Map",InverseGeneralMapping);
-DeclareSynonym("CompositionP1Map",CompositionMapping2);
-DeclareOperation("ConjugatedP1Map",[IsP1Map,IsP1Map]);
-DeclareOperation("ImageElm",[IsP1Map,IsP1Point]);
-DeclareOperation("PreImageElm",[IsP1Map,IsP1Point]);
-DeclareOperation("PreImagesElm",[IsP1Map,IsP1Point]);
-DeclareAttribute("CriticalPointsOfP1Map",IsP1Map);
-
+# undocumented for now --
+# given three maps alpha,beta,gamma,
+# find the pairs (s,t) in [0,1]^2 such that alpha(s)=beta(gamma(t))
 DeclareOperation("P1INTERSECT@",[IsP1Map,IsP1Map,IsP1Map]);
-DeclareOperation("P1ROTATION@",[IsP1Point,IsP1PointList,IsP1PointList]);
-DeclareGlobalFunction("P1MapRotatingP1Points");
-DeclareOperation("P1MapNormalizingP1Points",[IsP1PointList]);
-DeclareOperation("P1MapNormalizingP1Points",[IsP1PointList,IsP1PointList]);
-
 #############################################################################
 
 #E p1.gd . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .ends here
--- gap/sphere.gd
+++ gap/sphere.gd
@@ -10,42 +10,191 @@
 ##
 #############################################################################
 
+## <#GAPDoc Label="SphereGroups">
+## <ManSection>
+##   <Filt Name="IsSphereGroup"/>
+DeclareProperty("IsSphereGroup", IsFpGroup);
 DeclareCategory("IsElementOfSphereGroup", IsElementOfFpGroup and IsAssocWord);
 DeclareCategoryCollections("IsElementOfSphereGroup");
 DeclareCategoryCollections("IsElementOfSphereGroupCollection");
 DeclareProperty("IsElementOfSphereGroupFamily", IsElementOfFpGroupFamily);
-DeclareProperty("IsSphereGroup", IsFpGroup);
+DeclareOperation("ElementOfSphereGroup", [IsFamily, IsAssocWordWithInverse]);
+##   <Description>
+##     A sphere group is a special kind of finitely presented group, in which
+##     exactly one relation is a product, in some order, of all the
+##     generators, and all the other relations (possibly none)
+##     are powers of generators.
+##
+##     <P/> Sphere groups are used to represent the fundamental groups of
+##     punctured spheres, or more generally orbifolds whose underlying
+##     space is a sphere.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Attr Name="IsomorphismSphereGroup" Arg="g"/>
+##   <Attr Name="AsSphereGroup" Arg="g"/>
 DeclareAttribute("IsomorphismSphereGroup", IsFpGroup);
 DeclareAttribute("AsSphereGroup", IsFpGroup);
+##   <Description>
+##     These functions compute an isomorphism from <A>g</A> to a sphere group;
+##     the first form returns the isomorphism, while the second one returns
+##     its image.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Attr Name="EulerCharacteristic" Arg="g"/>
 DeclareAttribute("EulerCharacteristic", IsGroup);
+##   <Returns>The Euler characteristic of <A>g</A>.</Returns>
+##   <Description>
+##     The Euler characteristic of a free group of rank <M>n</M> is
+##     <M>1-n</M>; and it multiplies by the index on subgroups. A
+##     sphere group is finite if and only if its Euler characteristic is
+##     positive, and is virtually abelian if and only if its Euler
+##     characteristic is <M>0</M>.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Attr Name="RankOfSphereGroup" Arg="g"/>
 DeclareAttribute("RankOfSphereGroup", IsSphereGroup);
+##   <Returns>The number of generators of <A>g</A>.</Returns>
+## </ManSection
+##
+## <ManSection>
+##   <Attr Name="OrderingOfSphereGroup" Arg="g"/>
 DeclareAttribute("OrderingOfSphereGroup", IsSphereGroup);
+##   <Returns>The list of the orders of the generators.</Returns>
+##   <Description>
+##     This attribute has the property that
+##     <C>Product(GeneratorsOfGroup(g){OrderingOfSphereGroup(g)})</C> is the
+##     identity.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Attr Name="ExponentsOfSphereGroup" Arg="g"/>
 DeclareAttribute("ExponentsOfSphereGroup", IsSphereGroup);
+##   <Returns>The list of exponents of the generators.</Returns>
+##   <Description>
+##     This attribute has the property that
+##     <C>GeneratorsOfGroup(g)[i]^ExponentsOfSphereGroup(g)[i]</C> is the
+##     identity for all <C>i</C>. If an element has infinite order, the
+##     value stored is <C>0</C>.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Attr Name="IsomorphismFreeGroup" Arg="g"/>
 DeclareAttribute("IsomorphismFreeGroup", IsSphereGroup);
-
+##   <Returns>An isomorphism to a free group, if it exists.</Returns>
+##   <Description>
+##     If <A>g</A> was created as a sphere group with all exponents
+##     infinity, then <A>g</A> is isomorphic to a free group on all the
+##     generators but one; this attribute stores such an isomorphism.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Func Name="SphereGroup" Arg="ordering, [exponent]"/>
 DeclareGlobalFunction("SphereGroup");
-DeclareOperation("ElementOfSphereGroup", [IsFamily, IsAssocWordWithInverse]);
-
+##   <Returns>A new sphere group.</Returns>
+##   <Description>
+##     <A>ordering</A> is either a list of integers, describing the order
+##     of the generators that is to be trivial; or an integer <C>m</C>,
+##     in which case the ordering is <C>[m,m-1,..,1]</C>.
+##
+##     <P/> The optional second argument <A>exponent</A> is a list of
+##     integers describing the exponents of the generators. The value
+##     <C>0</C> specifies a generator of infinite order.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Filt Name="IsSphereConjugacyClass"/>
 DeclareProperty("IsSphereConjugacyClass", IsAssociativeElementCollection and IsMultiplicativeElementWithInverseCollection);
 DeclareProperty("IsSphereConjugacyClassCollection", IsAssociativeElementCollColl and IsMultiplicativeElementWithInverseCollColl);
 DeclareSynonym("IsMulticurve", IsSphereConjugacyClassCollection);
-DeclareAttribute("PeripheralClasses", IsSphereGroup);
-DeclareProperty("IsPeripheral", IsElementOfSphereGroup);
-DeclareProperty("IsPeripheral", IsSphereConjugacyClass);
 DeclareOperation("InverseMutable", [IsSphereConjugacyClass]);
 DeclareOperation("InverseSameMutability", [IsSphereConjugacyClass]);
 DeclareAttribute("InverseImmutable", IsSphereConjugacyClass);
 DeclareOperation("POW", [IsSphereConjugacyClass,IsInt]);
+##   <Description>
+##     Elements of a sphere group represent based loops on a punctured
+##     sphere. Loops (without specified basepoint) are represented by
+##     conjugacy classes. A <E>multicurve</E> is a collection of
+##     non-intersecting loops.
+##
+##     <P/> Conjugacy classes may be raised to integer powers; the <M>n</M>th
+##     power of a conjugacy class is the conjugacy class of the <M>n</M>th
+##     power of an element.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Prop Name="" Arg="c"/>
+DeclareProperty("IsPeripheral", IsElementOfSphereGroup);
+DeclareProperty("IsPeripheral", IsSphereConjugacyClass);
+##   <Returns>Whether the conjugacy class <A>c</A> is peripheral.</Returns>
+##   <Description>
+##     A conjugacy class is <E>peripheral</E> if it contains a generator of
+##     the sphere group.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Attr Name="PeripheralClasses" Arg="g"/>
+DeclareAttribute("PeripheralClasses", IsSphereGroup);
+##   <Returns>The peripheral conjugacy classes of <A>g</A>.</Returns>
+## </ManSection
+##
 
+## <ManSection>
+##   <Attr Name="IntersectionNumber" Arg="c, d"/>
 DeclareOperation("IntersectionNumber", [IsSphereConjugacyClass,IsSphereConjugacyClass]);
 DeclareOperation("SelfIntersectionNumber", [IsSphereConjugacyClass]);
-
+##   <Returns>The intersection number of the conjugacy classes <A>c</A> and <A>d</A>.</Returns>
+##   <Description>
+##     The <E>geometric intersection number</E> of two loops is the minimal
+##     number of intersections they may have. The <E>self-intersection
+##     number</E> of a loop is the intersection number of the loop with
+##     a small translate.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Attr Name="AutomorphismGroup" Arg="g"/>
+##   <Attr Name="EpimorphismToOut" Arg="a"/>
 DeclareProperty("IsAutomorphismGroupOfSphereGroup", IsAutomorphismGroup);
 InstallTrueMethod(IsAutomorphismGroup, IsAutomorphismGroupOfSphereGroup);
-
 DeclareAttribute("EpimorphismToOut", IsAutomorphismGroupOfSphereGroup);
-
+##   <Description>
+##     This function computes the <E>pure</E> automorphism group of the
+##     sphere group <A>g</A>, namely the group of automorphisms that preserves
+##     all the peripheral conjugacy classes (conjugacy classes of generators).
+##
+##     <P/> The attribute <C>EpimorphismToOut</C> stores an epimorphism
+##     from the automorphism group to the group of outer automorphisms.
+##   </Description>
+## </ManSection
+##
+## <ManSection>
+##   <Oper Name="AmalgamateFreeProduct" Arg="g, h, x, y"/>
 DeclareOperation("AmalgamatedFreeProduct", [IsSphereGroup,IsSphereGroup,IsElementOfSphereGroup,IsElementOfSphereGroup]);
 DeclareAttribute("EmbeddingsOfAmalgamatedFreeProduct", IsSphereGroup);
+##   <Description>
+##     This function computes the amalgamated free product of two
+##     sphere groups <A>g</A> and <A>h</A>, along the cyclic subgroups
+##     <M>\langle x\rangle</M> of <A>g</A> and <M>\langle y\rangle</M>
+##     of <A>h</A>.
+##
+##     <P/> The attribute <C>EmbeddingsOfAmalgamatedFreeProduct</C>
+##     is a list of length two, storing the embeddings of <A>g</A> and
+##     <A>h</A> respectively into the amalgam.
+##   </Description>
+## </ManSection
+##
+## <#/GAPDoc>
 
 #E sphere.gd . . . . . . . . . . . . . . . . . . . . . . . . . . . .ends here
